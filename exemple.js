915a5cbe182b2d593d845ecd2867bad06b934e93
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-09-23 19:21:33 +0200

    exemple

---
+++ exemple.js
@@ -0,0 +1,13 @@
+console.log("Hola");
+let div = window.document.getElementById("div_text");
+div.innerHTML = "Hola HTML";
+div.innerText+= "Hola <h1> HTML </h1>";
+ // alert("Popupfeo alert");
+ let input_numero = document.getElementById("input_numero");
+let numero = input_numero.value
+  let resultat = document.getElementById("resultat");
+  resultat.innerText = numero;
+
+  function sumaValors() {
+    resultat.innerText = input_numero.value;
+  }

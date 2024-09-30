dc900d4e0fbe8fbb62b5db73850886365df80ec6
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-09-30 18:37:02 +0200

    duration

--- exemple.js
+++ exemple.js
@@ -30,6 +30,32 @@ let numero = input_numero.value
     audio.src = "FANFARE1.WAV";
     audio.play();
   }
+  function clik_btn_stopAudio() {
+    audio.pause();
+    audio.currentTime=0;
+  }
+  function clik_btn_mute() {
+    audio.muted=true;
+  }
+  function clik_btn_volUp() {
+    try {
+      audio.volume +=  0.2;
+    } catch (e) {
+      audio.volume= 1;
+    }
+    document.getElementById("vol").value=audio.volume;
+  }
+  function clik_btn_volDown() {
+    try {
+      audio.volume -= 0.2;
+    } catch (e) {
+      audio.volume=0;
+    }
+    document.getElementById("vol").value=audio.volume;
+  }
+  function change_inp_vol() {
+    audio.volume = document.getElementById("vol").value;
+  }
   /**
    * Crea un document HTML amb un div amb id “llista_propietats”. Programa amb JS que es creï una
 llista amb els següents missatges i amb els valors obtinguts dinàmicament:
